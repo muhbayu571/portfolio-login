ed768c73053af2ac41865abc807c21fb6b698f6e
muhbayu571 <63641006+muhbayu571@users.noreply.github.com>
2020-08-07 02:01:04 +0700

    belajar

---
+++ script.js
@@ -0,0 +1,7 @@
+ $(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+    // $('.modal-trigger').click(function(){
+    // 	$('.card').css('top','-100px');
+    // })
+  });
