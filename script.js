0f04bd95e971bba90648b348e765b2704c304b2c
muhbayu571 <63641006+muhbayu571@users.noreply.github.com>
2020-08-07 03:34:30 +0700

    update js

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
  $(document).ready(function(){
     // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
-    // $('.modal-trigger').click(function(){
-    // 	$('.card').css('top','-100px');
-    // })
+    $('.icon-mata').click(function(){
+    	const text = $(this).text();
+    	if(text == "visibility"){
+    		$(this).text('visibility_off');
+    		$('.pass').attr('type','password');
+    	}else{
+    		$(this).text('visibility');
+    		$('.pass').attr('type','text');
+    	}
+    })
+    $('.modal-action').click(function(){
+    	const username=$('.username').val();
+    	const pass=$('.pass').val();
+    	alert('username = '+username+' || '+'password = '+pass);
+    })
   });
